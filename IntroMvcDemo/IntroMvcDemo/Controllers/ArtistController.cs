af7361eedc140fcc2cbd976d9e0f991f58a08055
Justin Wendlandt <jwendl@hotmail.com>
2016-05-27 16:43:32 -0500

    Added database and got castle.windsor working.

---
+++ IntroMvcDemo/IntroMvcDemo/Controllers/ArtistController.cs
@@ -0,0 +1,25 @@
+﻿using IntroMvcDemo.DataAccess.Interfaces;
+using IntroMvcDemo.DataAccess.Models;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace IntroMvcDemo.Controllers
+{
+    public class ArtistController
+        : Controller
+    {
+        private IDataRepository<Artist> artistRepository { get; set; }
+
+        public ArtistController(IDataRepository<Artist> artistRepository)
+        {
+            this.artistRepository = artistRepository;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var artist = await artistRepository.FetchAsync(1);
+
+            return View(artist);
+        }
+    }
+}
---
+++ IntroMvcDemo/IntroMvcDemo/Installers/RepositoriesInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using IntroMvcDemo.Common;
+using IntroMvcDemo.DataAccess.Interfaces;
+using System.Data.Entity;
+
+namespace IntroMvcDemo.Installers
+{
+    public class RepositoriesInstaller
+        : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            Arg.IsNotNull(() => container);
+            var dataAccessAssemblyName = "IntroMvcDemo.DataAccess";
+
+            container.Register(Classes.FromAssemblyNamed(dataAccessAssemblyName).BasedOn(typeof(DbContext)).WithService.Base().LifestylePerWebRequest());
+            container.Register(Classes.FromAssemblyNamed(dataAccessAssemblyName).BasedOn(typeof(IDataRepository<>)).WithService.Base().LifestylePerWebRequest());
+        }
+    }
+}
